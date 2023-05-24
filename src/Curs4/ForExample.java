e544e9a9ddb48756e7a8b48275bc01340ee67521
unknown <ovidiu.cioplea@gmail.com>
2023-05-24 17:43:23 +0300

    Curs 4 recuperare

---
+++ src/Curs4/ForExample.java
@@ -0,0 +1,19 @@
+package Curs4;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+
+		
+		for(int i=0; i<10;i++) {
+			if(i == 5) {
+				//break;//intrerupe executia buclei
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
---
+++ src/Curs4/Palnidrom.java
@@ -0,0 +1,34 @@
+package Curs4;
+
+public class Palnidrom {
+
+	public static void main(String[] args) {
+
+		//radar  
+		checkPalindrom("masina");
+		
+		
+	}
+	//radar
+	//01234 
+	//cuvant2 = ""
+	//"" = "" + r
+	//cuvant2 = "r"
+	//"r" = "r" + a
+	//cuvant2 = "ra"
+	//"ra" = "ra" + d
+	
+	public static void checkPalindrom(String cuvant) {
+		String cuvant2 = "";
+		
+		for(int i=cuvant.length()-1; i>=0;i--) {
+			
+			//cuvant2 += cuvant.charAt(i);
+			cuvant2 = cuvant2 + cuvant.charAt(i);
+		}
+		
+		String result = cuvant2.equals(cuvant) ? "Este palindorm" : "Nu este palindorm";
+		System.out.println(result);
+	}
+
+}
